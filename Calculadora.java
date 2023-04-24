63493047d0ba282753dcffa63d1a3ca38b72bfd8
efren21 <125391725+efren21@users.noreply.github.com>
2023-04-24 17:21:44 +0200

    Add files via upload
    Proyecto calculadora documentado

--- Calculadora.java
+++ Calculadora.java
@@ -3,56 +3,123 @@ package modelo;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * La clase Calculadora representa una calculadora básica que puede realizar
+ * operaciones aritméticas básicas como suma, resta, multiplicación y división.
+ */
 public class Calculadora {
+
+	/** El primer valor de la operación. */
 	private Float valor1;
+
+	/** El segundo valor de la operación. */
 	private Float valor2;
+
+	/** El resultado de la operación. */
 	private Float resultado;
+
+	/** La lista de resultados de las operaciones realizadas. */
 	private List<String> resultados = new ArrayList<String>();
 
+	/**
+	 * Obtiene el primer valor de la operación.
+	 * 
+	 * @return El primer valor de la operación.
+	 */
 	public Float getValor1() {
 		return valor1;
 	}
 
+	/**
+	 * Establece el primer valor de la operación.
+	 * 
+	 * @param valor1 El primer valor de la operación.
+	 */
 	public void setValor1(Float valor1) {
 		this.valor1 = valor1;
 	}
 
+	/**
+	 * Obtiene el segundo valor de la operación.
+	 * 
+	 * @return El segundo valor de la operación.
+	 */
 	public Float getValor2() {
 		return valor2;
 	}
 
+	/**
+	 * Establece el segundo valor de la operación.
+	 * 
+	 * @param valor2 El segundo valor de la operación.
+	 */
 	public void setValor2(Float valor2) {
 		this.valor2 = valor2;
 	}
 
+	/**
+	 * Obtiene el resultado de la operación.
+	 * 
+	 * @return El resultado de la operación.
+	 */
 	public Float getResultado() {
 		return resultado;
 	}
 
+	/**
+	 * Establece el resultado de la operación.
+	 * 
+	 * @param resultado El resultado de la operación.
+	 */
 	public void setResultado(Float resultado) {
 		this.resultado = resultado;
 	}
 
+	/**
+	 * Obtiene la lista de resultados de las operaciones realizadas.
+	 * 
+	 * @return La lista de resultados de las operaciones realizadas.
+	 */
 	public List<String> getResultados() {
 		return resultados;
 	}
 
+	/**
+	 * Establece la lista de resultados de las operaciones realizadas.
+	 * 
+	 * @param resultados La lista de resultados de las operaciones realizadas.
+	 */
 	public void setResultados(List<String> resultados) {
 		this.resultados = resultados;
 	}
 	
+	/**
+	 * Realiza la operación de suma entre los dos valores y agrega el resultado a la lista de resultados.
+	 */
 	public void somar(){
 		resultado=valor1+valor2;
 		resultados.add("["+valor1+"+"+valor2+"="+resultado+"]");
 	}
+	
+	/**
+	 * Realiza la operación de resta entre los dos valores y agrega el resultado a la lista de resultados.
+	 */
 	public void subtrair(){
 		resultado=valor1-valor2;
 		resultados.add("["+valor1+"-"+valor2+"="+resultado+"]");
 	}
+	
+	/**
+	 * Realiza la operación de multiplicación entre los dos valores y agrega el resultado a la lista de resultados.
+	 */
 	public void multiplicar(){
 		resultado=valor1*valor2;
 		resultados.add("["+valor1+"*"+valor2+"="+resultado+"]");
 	}
+	
+	/**
+	 * Realiza la operación de división entre los dos valores y agrega el resultado a la lista de resultados.
+	 */
 	public void dividir(){
 		resultado=valor1/valor2;
 		resultados.add("["+valor1+"/"+valor2+"="+resultado+"]");
--- MenuCalculadora.java
+++ MenuCalculadora.java
@@ -1,65 +1,85 @@
-package modelo;
-
-import java.util.Scanner;
-
-import javax.swing.JOptionPane;
-
+/**
+ * La clase MenuCalculadora es la clase principal que se encarga de ejecutar la calculadora y mostrar un menú con las opciones
+ * de sumar, restar, multiplicar, dividir e imprimir los resultados de las operaciones realizadas.
+ *
+ * @version 1.0
+ * @since 2022-04-24
+ */
 public class MenuCalculadora {
-	static Integer valorMenu = 6;
-	static Scanner teclado = new Scanner(System.in);	
-	public static void main(String[] args) {
-		Calculadora calculadora = new Calculadora();
-		while (valorMenu != 0) {
-			if (valorMenu == 1) {
-				Float numero1 = Float.parseFloat(JOptionPane.showInputDialog("Informe o valor 1"));
-				Float numero2 = Float.parseFloat(JOptionPane.showInputDialog("Informe o valor 2"));
-				calculadora.setValor1(numero1);
-				calculadora.setValor2(numero2);
-				calculadora.somar();
-			
-				chamarMenu();
-			}else if (valorMenu == 2) {
-				Float numero1 = Float.parseFloat(JOptionPane.showInputDialog("Informe o valor 1"));
-				Float numero2 = Float.parseFloat(JOptionPane.showInputDialog("Informe o valor 2"));
-				calculadora.setValor1(numero1);
-				calculadora.setValor2(numero2);
-				calculadora.subtrair();
-				chamarMenu();
-			}else if (valorMenu == 3) {
-				Float numero1 = Float.parseFloat(JOptionPane.showInputDialog("Informe o valor 1"));
-				Float numero2 = Float.parseFloat(JOptionPane.showInputDialog("Informe o valor 2"));
-				calculadora.setValor1(numero1);
-				calculadora.setValor2(numero2);
-				calculadora.multiplicar();
-				chamarMenu();
-			}else if (valorMenu == 4) {
-				Float numero1 = Float.parseFloat(JOptionPane.showInputDialog("Informe o valor 1"));
-				Float numero2 = Float.parseFloat(JOptionPane.showInputDialog("Informe o valor 2"));
-				calculadora.setValor1(numero1);
-				calculadora.setValor2(numero2);
-				calculadora.dividir();
-				chamarMenu();
-			} else if (valorMenu == 5) {
-				System.err.println(calculadora.getResultados());
-				chamarMenu();
-			} else if (valorMenu == 0) {
-				System.exit(0);
-			}else{
-				chamarMenu();
-			}
-		}
-	}
 
-	private static void chamarMenu() {		
-		System.err.println("#########################");
-		System.err.println("MENU");
-		System.err.println("1:SOMAR");
-		System.err.println("2:SUBTRAIR");
-		System.err.println("3:MULTIPLICAR");
-		System.err.println("4:DIVIDIR");
-		System.err.println("5:IMPRIMIR");
-		System.err.println("0:SAIR");
-		valorMenu = teclado.nextInt();
+    /**
+     * El valor de la opción seleccionada en el menú.
+     */
+    static Integer valorMenu = 6;
 
-	}
+    /**
+     * Objeto Scanner utilizado para leer la entrada del usuario desde la consola.
+     */
+    static Scanner teclado = new Scanner(System.in);	
+
+    /**
+     * Método principal que crea una instancia de la clase Calculadora y muestra el menú de opciones al usuario.
+     *
+     * @param args Argumentos pasados al programa (no se utilizan en este caso).
+     */
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        while (valorMenu != 0) {
+            if (valorMenu == 1) {
+                Float numero1 = Float.parseFloat(JOptionPane.showInputDialog("Informe o valor 1"));
+                Float numero2 = Float.parseFloat(JOptionPane.showInputDialog("Informe o valor 2"));
+                calculadora.setValor1(numero1);
+                calculadora.setValor2(numero2);
+                calculadora.somar();
+
+                chamarMenu();
+            } else if (valorMenu == 2) {
+                Float numero1 = Float.parseFloat(JOptionPane.showInputDialog("Informe o valor 1"));
+                Float numero2 = Float.parseFloat(JOptionPane.showInputDialog("Informe o valor 2"));
+                calculadora.setValor1(numero1);
+                calculadora.setValor2(numero2);
+                calculadora.subtrair();
+                chamarMenu();
+            } else if (valorMenu == 3) {
+                Float numero1 = Float.parseFloat(JOptionPane.showInputDialog("Informe o valor 1"));
+                Float numero2 = Float.parseFloat(JOptionPane.showInputDialog("Informe o valor 2"));
+                calculadora.setValor1(numero1);
+                calculadora.setValor2(numero2);
+                calculadora.multiplicar();
+                chamarMenu();
+            } else if (valorMenu == 4) {
+                Float numero1 = Float.parseFloat(JOptionPane.showInputDialog("Informe o valor 1"));
+                Float numero2 = Float.parseFloat(JOptionPane.showInputDialog("Informe o valor 2"));
+                calculadora.setValor1(numero1);
+                calculadora.setValor2(numero2);
+                calculadora.dividir();
+                chamarMenu();
+            } else if (valorMenu == 5) {
+                System.err.println(calculadora.getResultados());
+                chamarMenu();
+            } else if (valorMenu == 0) {
+                System.exit(0);
+            } else {
+                chamarMenu();
+            }
+        }
+    }
+
+    /**
+     * Este método muestra el menú de opciones en la consola y espera a que el usuario ingrese una opción.
+     * Si la opción ingresada no es válida, se vuelve a mostrar el menú.
+     */
+    private static void chamarMenu() {
+        System.err.println("#########################");
+        System.err.println("MENU");
+        System.err.println("1:SOMAR");
+        System.err.println("2:SUBTRAIR");
+        System.err.println("3:MULTIPLICAR");
+        System.err.println("4:DIVIDIR");
+        System.err.println("5:IMPRIMIR");
+        System.err.println("0:SAIR");
+
+        valorMenu = teclado.nextInt();
+    }
 }
